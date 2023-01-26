0c2c62f3d2ad6a84cd478bc9394ad20e2d800e60
ArtemTiago <tiagoartem@gmail.com>
2023-01-26 17:13:00 -0300

    Carrossel consertado

--- carrossel.js
+++ carrossel.js
@@ -1,4 +1,4 @@
-document.getElementsByClassName('next').onclick = function(){
+ /*document.getElementsByClassName('next').onclick = function(){
     const widthItem = document.querySelector('.item').offsetWidth;
     document.getElementsByClassName('formList').scrollLeft += widthItem;
     }
@@ -7,3 +7,34 @@ document.getElementsByClassName('next').onclick = function(){
         const widthItem = document.querySelector('.item').offsetWidth;
         document.getElementsByClassName('formList').scrollLeft -= widthItem;
         }
+
+        /*
+function previous(){
+    document.write("asjdkhasjdkhsa")
+
+}
+*/
+document.getElementById('next').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList').scrollLeft += widthItem;
+
+}
+
+document.getElementById('prev').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList').scrollLeft -= widthItem;
+
+}
+
+
+document.getElementById('next2').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList2').scrollLeft += widthItem;
+
+}
+
+document.getElementById('prev2').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList2').scrollLeft -= widthItem;
+
+}
